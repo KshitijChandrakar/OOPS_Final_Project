84e84212b29a8f2dba308c173fbfd4cd886548f0
Kshitij_Chandrakar <kshitijchandrakar2019@gmail.com>
2025-04-23 16:25:09 +0530

    Pushing-content

---
+++ Space Invaders/src/Game/Main.java
@@ -0,0 +1,10 @@
+package Game;
+
+import MainMenu.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        MainFrame mainFrame=new MainFrame();
+    }
+}
